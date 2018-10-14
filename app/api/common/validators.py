dc9e2bd557096082ecbc746103e2bed978acda42
kwanj-k <mwangikelvin23@.com>
2018-10-14 08:35:43 +0300

    [Feature #161199165] Validate user inputs

---
+++ app/api/common/validators.py
@@ -0,0 +1,43 @@
+"""
+File with all user input validation methods
+"""
+
+
+from flask import abort
+
+def space_stripper(k):
+    """ Method to remove white space from inputs."""
+    for i,v in k.items():
+        try:
+            stripped = "".join(v.split())
+            if stripped == "":
+                msg = 'The field {} cannot be empty'.format(i)
+                abort(406,msg)
+        except:
+            continue
+
+def product_validator(k):
+    pay_load = ['name','inventory','price']
+    for i in k.keys():
+        if i not in pay_load:
+            msg = 'Please provide name,inventory and price of the product only'
+            abort(400, msg)
+    for i,v in k.items():
+        if i == 'name':
+            if v.isdigit():
+                msg = 'Name of the product can not be an integer'
+                abort(400,msg)
+            s = "".join(v.split())
+            if s == "":
+                msg = 'The product {} cannot be empty'.format(i)
+                abort(406,msg)
+        if i == 'inventory' or i == 'price':
+            try:
+                int(v)
+            except:
+                msg = 'Please make sure the {} is a number'.format(i)
+                abort(400,msg)         
+    for i in pay_load:
+        if i not in k.keys():
+            msg = 'Please provide the {} of the product'.format(i)
+            abort(400,msg)
--- app/api/v1/views/products_views.py
+++ app/api/v1/views/products_views.py
@@ -8,6 +8,7 @@ from flask_restplus import Resource,Namespace
 from app.api.v1.models.products import Product
 from app.api.v1.models.db import Db
 from app.api.v1.views.expect import ProductEtn
+from app.api.common.validators import product_validator
 
 new_p = ProductEtn().products
 v1 = ProductEtn().v1
@@ -17,6 +18,7 @@ class Products(Resource):
     @v1.expect(new_p)
     def post(self):
         json_data = request.get_json(force=True)
+        product_validator(json_data)
         new_product = Product(json_data['name'],
                             json_data['inventory'],
                             json_data['price'])
