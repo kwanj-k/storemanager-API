f832629c868770aeb36df55a09fec843a7025d19
kwanj-k <mwangikelvin23@.com>
2018-10-15 11:37:09 +0300

    [Feature  #161210690] Add all the account related models

---
+++ app/api/v1/models/accounts.py
@@ -0,0 +1,98 @@
+"""
+This file contains all the account models
+Store
+SuperAdmin
+Admin
+Attendant
+"""
+
+
+#Third party import
+from werkzeug.security import generate_password_hash
+
+"""
+pk :variable used to generate ids 
+"""
+
+class Store:
+    pk  = 1
+    def __init__(self, name,category):
+        self.id = Store.pk
+        self.name = name
+        self.category = category
+        Store.pk += 1
+
+    def json_dump(self):
+        """
+        custom json_dump method to return a custom python dict in response
+        """
+        return dict(
+            store_name = self.name,
+            category = self.category
+        )
+
+class User:
+    pk = 1
+    
+    def __init__(self,username,email,store_id,password):
+        self.id = User.pk
+        self.store_id = store_id
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+        User.pk += 1
+
+class SuperAdmin(User):
+    role = 0
+
+    def __init__(self, username, email, store_id, password):
+        super().__init__(username=username, 
+            email=email, store_id=store_id, password=password)
+        self.role = SuperAdmin.role
+
+    def json_dump(self):
+        """
+        custom json_dump method to return a custom python dict in response
+        """
+        return dict(
+            username = self.username,
+            email = self.email,
+            role = 'SuperAdmin'
+        )
+
+class Admin(User):
+    role = 2
+
+    def __init__(self,store_id, username, email, password):
+        super().__init__(store_id=store_id, 
+            username=username, email=email, password=password)
+        self.role = Admin.role
+
+    def json_dump(self):
+        """
+        custom json_dump method to return a custom python dict in response
+        """
+        return dict(
+            username = self.username,
+            email = self.email,
+            role = 'Admin'
+        )
+
+class Attendant(User):
+    role = 3
+
+    def __init__(self,store_id, username, email, password):
+        super().__init__(store_id=store_id, username=username, email=email, password=password)
+        self.role = Attendant.role
+
+    def json_dump(self):
+        """
+        custom json_dump method to return a custom python dict in response
+        """
+        return dict(
+            username=self.username,
+            email=self.email,
+            role = 'Attendant')
+
+
+
