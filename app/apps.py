f90846bd3feb55bfc07c43dbe211c52ac96f3b3c
kwanj-k <mwangikelvin23@.com>
2018-10-17 09:54:00 +0300

    [Release #161256506] Catch jwt 500 error when no token is provided

--- app/apps.py
+++ app/apps.py
@@ -16,12 +16,16 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.url_map.strict_slashes = False
     # TODO change and hide the secret key
-    app.config['JWT_SECRET_KEY'] = 'super-secret-key-that'
+    app.config['JWT_SECRET_KEY'] = 'e9i%i6o%)pu05m7&&2)mvwt7q!7lj_r+q@pbi5#0835g3&@=a1'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=72)
-    app.config['PROPAGATE_EXCEPTIONS'] =True
+    
+
     jwt.init_app(app)
+    
 
     from .api.v1.routes import v_1 as v1
+    from .api.v1.routes import v1 as jwtapi
     app.register_blueprint(v1)
+    jwt._set_error_handler_callbacks(jwtapi)
 
     return app
