aa4885107840460bbb2db9d3b2447fa811134ade
kwanj-k <mwangikelvin23@.com>
2018-10-18 19:51:13 +0300

    [Finishes #161256506] Code refactor

--- app/tests/v1/test_auth.py
+++ app/tests/v1/test_auth.py
@@ -47,6 +47,8 @@ class TestAuth(Settings):
                             data=json.dumps(self.data),
                             headers=dict(Authorization="Bearer " + token),
                             content_type='application/json')
+        res1 = json.loads(res.data.decode())
+        self.assertEqual(res1['status'],'Success!')
         self.assertEqual(res.status_code, 201)
 
     def test_addattendant(self):
